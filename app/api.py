6a3fc191cfdb2f89ab1e96af8a9e72ba0e19df89
RafaelAlvesA <rafaeldosangelos@gmail.com>
2024-01-02 15:09:09 -0300

    coomit base

---
+++ app/api.py
@@ -0,0 +1,23 @@
+'''
+Codigo que eu uso para enviar respotas manualmente p lucio
+'''
+
+import requests
+import json
+
+data = {
+    "identifier": '35611694000188',
+    "name": ' 49 educaçao'
+}
+print(data)
+json_data = json.dumps(data)
+url = "https://inscricao.anprotec.org.br/access/new_associate.php"
+response = requests.post(url, data=json_data)
+
+if response.status_code == 200:
+    print("JSON enviado com sucesso!")
+    print(response.text)
+else:
+    print("Erro ao enviar o JSON para a API.")
+    print("Código de status:", response.status_code)
+    print(response.text)
