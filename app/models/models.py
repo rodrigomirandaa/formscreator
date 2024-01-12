df73f6a7b5972af57bec3eb80e47ad73ceb72a54
Amedir <ademirmsfilho@fac.pe.senac.br>
2024-01-11 22:06:50 -0300

    Feat: Cria função para upar arquivos no supabase

--- app/models/models.py
+++ app/models/models.py
@@ -1,10 +1,14 @@
+from datetime import datetime
 from app.api.questions.utils import get_json_form, serealizer_json_forms
 from app.models.notion import DatabaseNotion
+from app.extensions import supabase
+import uuid
 import json
+import io
 
 class Forms():
-    def __init__(self, id, title, description, properties):
-        self.id = id
+    def __init__(self, title, description, properties):
+        self.id = str(uuid.uuid4())
         self.title = title
         self.description = description
         self.properties = properties
@@ -22,6 +26,19 @@ class Forms():
 
         return form_data
     
+    def send_json_to_supabase(self):
+        file_object = io.BytesIO(json.dumps(self.serealizer()).encode())
+        # Obtenha o conteúdo dos bytes
+        file_content = file_object.getvalue()
+
+        res = supabase.storage.from_("json_bucket").upload(
+            file=file_content,
+            path=f"schemas/{self.id}.json",
+            file_options={"content-type": "application/json"}
+        )
+
+        return res
+
     def save(self):
         payload = {
             "properties": {
@@ -50,6 +67,6 @@ class Forms():
         conn = DatabaseNotion(databaseId="f6af625399864626834be3ec99927ed9")
         res = DatabaseNotion.post(payload=payload)
 
-    def delete(self):
-        db_session.delete(self)
-        db_session.commit()
+    # def delete(self):
+    #     db_session.delete(self)
+    #     db_session.commit()
